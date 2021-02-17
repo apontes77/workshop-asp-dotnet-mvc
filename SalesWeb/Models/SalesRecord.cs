f91f1abb1f0eda010cc38f1b0368d2760abb690a
alexandre.queiroz <alexandre.queiroz@siagri.com.br>
2021-02-17 18:19:22 -0300

    primeira versao do app terminada

--- SalesWeb/Models/SalesRecord.cs
+++ SalesWeb/Models/SalesRecord.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesWeb.Models
 
@@ -6,6 +7,7 @@ namespace SalesWeb.Models
     public class SalesRecord
     {
         public int Id { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
 
         public double Amount { get; set; }
