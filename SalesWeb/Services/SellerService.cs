9d84c729e8fd4a3406744ae7af6b29450a464669
alexandre.queiroz <alexandre.queiroz@siagri.com.br>
2021-02-17 10:03:37 -0300

    edicao de vendedor e excecoes peronalizadas

--- SalesWeb/Services/SellerService.cs
+++ SalesWeb/Services/SellerService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SalesWeb.Data;
 using SalesWeb.Models;
+using SalesWeb.Services.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -40,6 +41,27 @@ namespace SalesWeb.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update ( Seller obj )
+        {
+            if (_context.Seller.Any())
+            {
+               if (_context.Seller.Any(x => x.Id == obj.Id))
+                {
+                    throw new NotFoundException("Id not found");
+                }
+               try
+                {
+                    _context.Update(obj);
+                    _context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException e)
+                {
+                    throw new DbConcurrencyException(e.Message);
+                }
+                
+            }
         }
     }
 }
